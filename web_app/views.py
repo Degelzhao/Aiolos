0b77af2d8b4e09c3c225887d0b790801cc2cc3aa
degel·zhao <barryember@outlook.com>
2018-11-20 16:30:22 +0800

    Django day 7

--- web_app/views.py
+++ web_app/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -23,3 +26,37 @@ def topic(request, topic_id):
     # 将主题和条目存储在字典context中
     context = {'topic':topic, 'entries':entries}
     return render(request, 'web_app/topic.html', context)
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据: 创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('web_app:topics'))
+
+    context = {'form': form}
+    return render(request, 'web_app/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """在特定的主题中添加新条目"""
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据: 创建一个空表单
+        form = EntryForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(data = request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit = False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('web_app:topic', args = [topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'web_app/new_entry.html', context)
