aff59311f5727c4b9362403753be2d5d924fa333
Aiolos <barryember@outlook.com>
2018-12-08 11:09:24 +0800

    modify

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -145,7 +145,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
     # 支持所有主机头(host header)
-    ALLOWED_HOSTS = ['aiolos-one.herokuapp.com']
+    ALLOWED_HOSTS = ['aiolosone.herokuapp.com']
 
     DEBUG = False
 
